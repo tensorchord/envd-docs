91980f72a4131b842b23c69f8b6867a474154cc9
Ce Gao <ce.gao@outlook.com>
2022-06-09 17:05:37 +0800

    chore: Add CLI ref (#9)
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

---
+++ hack/generate-cli-ref.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/tensorchord/envd/pkg/app"
+)
+
+func main() {
+	app := app.New()
+	content, err := app.ToMarkdown()
+	if err != nil {
+		panic(err)
+	}
+	header := `---
+sidebar_position: 2
+---
+
+# envd CLI Reference
+
+`
+
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	if err := os.WriteFile(
+		filepath.Join(dir, "docs/reference/cli.md"), []byte(header+content), 0644); err != nil {
+		panic(err)
+	}
+	logrus.Info("cli reference generated")
+}
