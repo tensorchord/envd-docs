3434be3343177360661912c62cbfb1e808b77b29
Yuan Tang <terrytangyuan@gmail.com>
2022-06-11 20:07:40 -0400

    fix: Incorrect description metadata tag (#17)

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="envd: Development Environment for Data Scientists">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
