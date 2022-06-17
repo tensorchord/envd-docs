28c5a87f4db89c64721b86645c256055706e3012
Jinjing.Zhou <allenzhou@tensorchord.ai>
2022-06-17 15:10:59 +0800

    fix render
    Signed-off-by: Jinjing.Zhou <allenzhou@tensorchord.ai>

--- hack/api/render.py
+++ hack/api/render.py
@@ -1,3 +1,4 @@
+import dataclasses
 import json
 from pathlib import Path
 
@@ -7,9 +8,15 @@ from pydoc_markdown.contrib.processors.crossref import CrossrefProcessor
 from pydoc_markdown.contrib.processors.filter import FilterProcessor
 from pydoc_markdown.contrib.processors.smart import SmartProcessor
 from pydoc_markdown.contrib.processors.google import GoogleProcessor
-from pydoc_markdown.contrib.renderers.docusaurus import DocusaurusRenderer
+from pydoc_markdown.contrib.renderers.docusaurus import DocusaurusRenderer, CustomizedMarkdownRenderer
 from pydoc_markdown.interfaces import Context
 
+@dataclasses.dataclass
+class EnvdMkRender(CustomizedMarkdownRenderer):
+    escape_html_in_docstring = False
+
+render = EnvdMkRender()
+render.escape_html_in_docstring = False
 
 def main():
     base_path = Path(__file__).parent
@@ -21,9 +28,11 @@ def main():
         renderer=DocusaurusRenderer(
             docs_base_path=str(docs_path.resolve()),
             sidebar_top_level_label="API reference",
-            relative_output_path="./api"
+            relative_output_path="./api",
+            markdown=render
         ),
     )
+    config.renderer.escape_html_in_docstring = False
     modules = config.load_modules()
     config.process(modules)
     # patch __init__.py file
