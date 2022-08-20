f9fdf28b842cb17a0d473c0f69b9cecb27007819
Sepush <sepush@outlook.com>
2022-08-20 10:47:55 +0800

    chore: enhance type
    Signed-off-by: Sepush <sepush@outlook.com>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,3 +1,4 @@
+import { EnhanceAppContext } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import CustomTitle from '../components/CustomTitle.vue'
 import Author from '../components/Author.vue'
@@ -5,7 +6,7 @@ import "./custom.css"
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }) {
+  enhanceApp({ app }: EnhanceAppContext) {
     app
       .component('CustomTitle', CustomTitle)
       .component('Author', Author)
