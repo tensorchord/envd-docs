443a0fe9117c719bd7057d66a221122c660c582b
Ce Gao <cegao@tensorchord.ai>
2022-09-27 15:20:40 +0800

    fix: Fix typo
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- docs/.vitepress/config/sidebar/blog.ts
+++ docs/.vitepress/config/sidebar/blog.ts
@@ -8,7 +8,7 @@ export const blogSidebar: DefaultTheme.Sidebar = {
         // if you add a new blog post, add it to the sidebar here
         // eg: { text: 'Blog', link: '/blog/post1' },
         { text: 'Machine Learning Environment Should Be Easy', link: '/blog/ml-env' },
-        { test: 'envd is participating hacktoberfest!', link: '/blog/hacktoberfest' },
+        { text: 'envd Is Participating Hacktoberfest!', link: '/blog/hacktoberfest' },
       ]
     }
   ],
