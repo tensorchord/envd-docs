68c4e24fd18e70ec36e995c8aaf84d89ab28e93d
Ce Gao <ce.gao@outlook.com>
2022-10-25 13:22:01 +0800

    feat: Add blog for tea hour (#169)
    * feat: Add ca
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * fix: Add entry
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- docs-zh/.vitepress/config/sidebar/blog.ts
+++ docs-zh/.vitepress/config/sidebar/blog.ts
@@ -5,6 +5,7 @@ export const blogSidebar: DefaultTheme.Sidebar = {
     {
       text: '博客',
       items: [
+        { text: 'TensorChord Tea Hour: Cellular Automaton', link: '/blog/tea-hour-2'},
         { text: '2022 年黑客啤酒节 Hacktoberfest 开始，与 envd 社区一起参加！', link: '/blog/hacktoberfest'},
         { text: '使用 CPU 体验最顶尖的 AI 生成艺术模型', link: '/blog/stable-diffusion-cpu'},
         { text: 'envd: 配置深度学习炼丹环境的新选择', link: '/blog/envd'},
