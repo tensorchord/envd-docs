bb8231162bdc094c2df80b6f097d23849d551b17
Keming <kemingy94@gmail.com>
2022-12-14 20:51:39 +0800

    doc: add v1 doc, refactor render logic (#206)
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- hack/api/render.py
+++ hack/api/render.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 import shutil
-import io
+import os
 
 
 from pydoc_markdown import PydocMarkdown
@@ -16,32 +16,29 @@ from pydoc_markdown.contrib.renderers.docusaurus import (
 )
 
 
-def main():
-    base_path = Path(__file__).parent
-    repo_root = base_path / ".." / ".."
-    docs_path = repo_root / "docs/api/starlark"
-
+def render_doc(path: Path, output_dir: Path, enable_prefix: bool = False):
+    """Generate the markdown files"""
     config = PydocMarkdown(
-        loaders=[PythonLoader(search_path=[base_path], packages=["api", "lib"])],
+        loaders=[PythonLoader(search_path=[path])],
         processors=[
-            FilterProcessor(skip_empty_modules=False),
+            FilterProcessor(skip_empty_modules=True),
             CrossrefProcessor(),
             GoogleProcessor(),
             SmartProcessor(),
         ],
         renderer=DocusaurusRenderer(
-            docs_base_path=base_path / "docs",
+            docs_base_path=output_dir,
             markdown=CustomizedMarkdownRenderer(
                 escape_html_in_docstring=False,
                 render_module_header_template="",
                 header_level_by_type={
                     "Module": 1,
                     "Class": 2,
-                    "Method": 3,
+                    "Method": 4,
                     "Function": 2,
-                    "Variable": 3,
+                    "Variable": 4,
                 },
-                add_module_prefix=False,
+                add_module_prefix=enable_prefix,
             ),
         ),
     )
@@ -49,42 +46,57 @@ def main():
     config.process(modules)
     config.render(modules)
 
-    generated_doc_dir = base_path / "docs/reference"
 
-    sidebar = {"text": "API Reference"}
+def update_global_func(path: Path):
+    """Rename __init__ file to a upper level global func file"""
+    init = path / "__init__"
+    if (init).is_dir():
+        shutil.copyfile(init / "__init__.md", path / "global.md")
+        shutil.rmtree(init)
+
+
+def generate_sidebar(title: str, path: Path, target_dir: Path):
+    """Generate vitepress sidebar file"""
+    sidebar = {"text": title, "type": "category"}
     items = []
+    for file in os.listdir(path):
+        if file.endswith(".json"):
+            (path / file).unlink()
+            continue
+        name = file.removesuffix(".md")
+        items.append(
+            {
+                "text": name,
+                "link": f"{target_dir}/{name}",
+            }
+        )
+
+    sidebar["items"] = items
+    return sidebar
+
+
+def main():
+    base_path = Path(__file__).parent
+    repo_root = base_path / ".." / ".."
+    sidebar_path = (
+        repo_root / "docs" / ".vitepress" / "config" / "sidebar" / "apiSidebar.json"
+    )
+    docs_path = repo_root / "docs" / "api" / "starlark"
     docs_path.mkdir(exist_ok=True, parents=True)
 
-    for f in sorted((generated_doc_dir / "api").rglob("*.md")):
-        if f.name == "__init__.md":
-            parent_name = f.parent.name
-            if parent_name == "api":
-                # Means global functions
-                filename = "global_functions.md"
-                title = "global functions"
-            else:
-                filename = f"{parent_name}.md"
-                title = f"{parent_name} modules"
-            shutil.copy(f, docs_path / filename)
-            name = filename.replace(".md", "")
-            items.append({"text": title, "link": f"api/starlark/{name}"})
+    items = []
+    for module in ["v0", "lib"]:
+        md_dir = base_path / "docs"
+        ref_dir = md_dir / "reference"
+        render_doc(base_path / module, md_dir)
+        update_global_func(ref_dir)
+        items.append(generate_sidebar(module, ref_dir, f"api/starlark/{module}"))
+        shutil.copytree(ref_dir, docs_path / module, dirs_exist_ok=True)
+        shutil.rmtree(ref_dir)
 
-    lib_content = io.StringIO()
-    for f in sorted((generated_doc_dir / "lib").rglob("*.md")):
-        if f.name == "__init__.md":
-            continue
-        with open(f, "r") as reader:
-            for line in reader:
-                if not line.startswith("# envd"):
-                    lib_content.writelines(line)
-    with open(docs_path / "envdlib.md", "w") as writer:
-        writer.write(lib_content.getvalue())
-    items.append({"text": "envdlib", "link": "api/starlark/envdlib"})
-
-    sidebar = {"text": "API Reference", "items": items}
-    json.dump(
-        sidebar, open(repo_root / "docs/.vitepress/config/sidebar/apiSidebar.json", "w")
-    )
+    sidebar = {"text": "API Reference", "type": "category", "items": items}
+    with open(sidebar_path, "w") as f:
+        json.dump(sidebar, f, indent=2)
 
 
 if __name__ == "__main__":
