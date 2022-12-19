9d69c2ef607a2ed283a1290306816426e36b64b7
Keming <kemingy94@gmail.com>
2022-12-19 11:05:35 +0800

    add pycon china 2022 slides (#218)
    Signed-off-by: Keming <kemingyang@tensorchord.ai>
    
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- docs/.vitepress/config/sidebar/blog.ts
+++ docs/.vitepress/config/sidebar/blog.ts
@@ -10,6 +10,7 @@ export const blogSidebar: DefaultTheme.Sidebar = {
         { text: 'Machine Learning Environment Should Be Easy', link: '/blog/ml-env' },
         { text: 'envd Is Participating Hacktoberfest!', link: '/blog/hacktoberfest' },
         { text: '5 minutes to run your ML/AI environment on your remote machine', link: '/blog/envd-on-remote-serve' },
+        { text: 'PyCon China 2022 envd sharing', link: '/blog/pycon2022.md' }
       ]
     }
   ],
