74c67bc0b7fb810a715d209f071c75d9556560ff
Ce Gao <cegao@tensorchord.ai>
2023-01-03 18:18:58 +0800

    chore: Add 2022 post (#237)
    * fix: Update
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * fix: Add item
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * fix: Update
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * chore: Add english
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * Update docs/blog/2022.md
    
    Co-authored-by: Keming <kemingy94@gmail.com>
    
    * Update docs/blog/2022.md
    
    Co-authored-by: Jinjing Zhou <VoVAllen@users.noreply.github.com>
    
    * fix: Update
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    * fix: Fix
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>
    Co-authored-by: Keming <kemingy94@gmail.com>
    Co-authored-by: Jinjing Zhou <VoVAllen@users.noreply.github.com>

--- docs-zh/.vitepress/config/sidebar/blog.ts
+++ docs-zh/.vitepress/config/sidebar/blog.ts
@@ -5,6 +5,7 @@ export const blogSidebar: DefaultTheme.Sidebar = {
     {
       text: '博客',
       items: [
+        { text: 'TensorChord: 2022 in Review', link: '/blog/2022'},
         { text: 'TensorChord Tea Hour: Cellular Automaton', link: '/blog/tea-hour-2'},
         { text: '2022 年黑客啤酒节 Hacktoberfest 开始，与 envd 社区一起参加！', link: '/blog/hacktoberfest'},
         { text: '使用 CPU 体验最顶尖的 AI 生成艺术模型', link: '/blog/stable-diffusion-cpu'},
--- docs/.vitepress/config/sidebar/blog.ts
+++ docs/.vitepress/config/sidebar/blog.ts
@@ -7,6 +7,7 @@ export const blogSidebar: DefaultTheme.Sidebar = {
       items: [
         // if you add a new blog post, add it to the sidebar here
         // eg: { text: 'Blog', link: '/blog/post1' },
+        { text: 'TensorChord: 2022 in Review', link: '/blog/2022' },
         { text: 'Machine Learning Environment Should Be Easy', link: '/blog/ml-env' },
         { text: 'envd Is Participating Hacktoberfest!', link: '/blog/hacktoberfest' },
         { text: '5 minutes to run your ML/AI environment on your remote machine', link: '/blog/envd-on-remote-serve' },
