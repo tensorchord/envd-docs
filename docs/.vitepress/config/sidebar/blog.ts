8afa59d89c643b00025bd57e6728ac2fbaafa06f
Keming <kemingyang@tensorchord.ai>
2025-12-02 09:50:57 +0800

    blog: agent in the container
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- docs/.vitepress/config/sidebar/blog.ts
+++ docs/.vitepress/config/sidebar/blog.ts
@@ -7,6 +7,7 @@ export const blogSidebar: DefaultTheme.Sidebar = {
       items: [
         // if you add a new blog post, add it to the sidebar here
         // eg: { text: 'Blog', link: '/blog/post1' },
+        { text: 'Why the code agents should run inside the containers', link: '/blog/agent-container.md' },
         { text: 'Develop in the Kubernetes cluster', link: '/blog/envd-server.md' },
         { text: 'TensorChord: 2022 in Review', link: '/blog/2022' },
         { text: 'PyCon China 2022 envd sharing', link: '/blog/pycon2022.md' },
